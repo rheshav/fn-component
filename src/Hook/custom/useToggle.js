c6ca7afea80893333f8f09cd0771351b70a214c6
rheshav <rhesa.havilah@gmail.com>
2022-05-21 14:45:33 +0700

    add

---
+++ src/Hook/custom/useToggle.js
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+
+export const useToggle = () => {
+  const [value, setValue] = useState(false);
+  return [value, setValue];
+};
--- src/Hook/index.jsx
+++ src/Hook/index.jsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+import { useToggle } from './custom/useToggle';
 import Counter from './pembahasan/Counter';
 import Effect from './pembahasan/Effect';
 import Identity from './pembahasan/Identity';
 
 const Hooks = () => {
+  let [label, setLabel] = useState('ON');
+  let [lampu, setLampu] = useToggle();
+
+  const style = {
+    background: lampu ? 'yellow' : 'black',
+    textAlign: 'center',
+    height: '400px',
+  };
+  const saklar = () => {
+    setLampu(!lampu);
+    setLabel((e) => {
+      if (e === 'ON') {
+        return 'OFF';
+      } else return 'ON';
+    });
+  };
+
   return (
-    <div>
-      <br></br>
-      <Identity />
+    <div style={style}>
+      <button onClick={saklar}>{label}</button>
     </div>
   );
 };
-
 export default Hooks;
